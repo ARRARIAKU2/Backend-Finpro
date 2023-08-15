c0e379d0f26f0ccb0e18acf4cd8f6aed8bf1dcac
ARRARIAKU2 <yerry_kecak@yahoo.com>
2023-08-15 13:40:46 +0700

    as

--- index.js
+++ index.js
@@ -11,10 +11,7 @@ const mongooseString = process.env.DATABASE_URL;
 
 const app = express();
 
-mongoose.connect(mongooseString, { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true 
-});
+mongoose.connect(mongooseString);
 
 const db = mongoose.connection;
 db.on('error', (error) => {
@@ -29,6 +26,39 @@ app.use(express.json());
 app.use(UserRoute);
 app.use(VideoRoute);
 
-app.listen(5000, () => {
-    console.log('Server running on port 5000');
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log('Server running on port ', port);
+});
+import dotenv from 'dotenv';
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import UserRoute from './routes/UserRoute.js';
+import VideoRoute from './routes/VideoRoute.js';
+
+dotenv.config();
+
+const mongooseString = process.env.DATABASE_URL;
+
+const app = express();
+
+mongoose.connect(mongooseString);
+
+const db = mongoose.connection;
+db.on('error', (error) => {
+    console.error(error)
+});
+db.once('connected', () => {
+    console.log('Connected to database')
+})
+
+app.use(cors());
+app.use(express.json());
+app.use(UserRoute);
+app.use(VideoRoute);
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log('Server running on port ', port);
 });
