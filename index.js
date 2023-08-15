e3a30ea0edc403753f11ff90cc3cb77d28a50e0e
ARRARIAKU2 <yerry_kecak@yahoo.com>
2023-08-15 13:41:10 +0700

    as

--- index.js
+++ index.js
@@ -30,35 +30,4 @@ const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log('Server running on port ', port);
 });
-import dotenv from 'dotenv';
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import UserRoute from './routes/UserRoute.js';
-import VideoRoute from './routes/VideoRoute.js';
 
-dotenv.config();
-
-const mongooseString = process.env.DATABASE_URL;
-
-const app = express();
-
-mongoose.connect(mongooseString);
-
-const db = mongoose.connection;
-db.on('error', (error) => {
-    console.error(error)
-});
-db.once('connected', () => {
-    console.log('Connected to database')
-})
-
-app.use(cors());
-app.use(express.json());
-app.use(UserRoute);
-app.use(VideoRoute);
-
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-    console.log('Server running on port ', port);
-});
